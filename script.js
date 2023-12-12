c9470c28f9d2bc76fe62d2f71bbb5c80f2a662e9
Hari Priya Chalasani <priyachalasani1@gmail.com>
2023-12-12 20:16:12 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,12 @@
+// selecting all the details
+
+const dragArea = document.querySelector(".drag-area");
+header = dragArea.querySelector("h1");
+button = dragArea.querySelector("button");
+input = dragArea.querySelector("input");
+
+let file;
+
+button.onclick = () => {
+    input.click(); 
+}
